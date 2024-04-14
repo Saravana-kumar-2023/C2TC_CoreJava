269519be9227f857da086422642ce95a46a20f9b
Saravana-kumar-2023 <146946115+Saravana-kumar-2023@users.noreply.github.com>
2024-04-14 17:00:33 +0530

    First Commit

---
+++ src/com/Saravanakumar/dayone/ChocolateGame.java
@@ -0,0 +1,17 @@
+package com.Saravanakumar.dayone;
+import java.util.Scanner;
+public class ChocolateGame {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a + b;
+        if(sum%2==0) {
+        	System.out.println("No");
+        }
+        else {
+        	System.out.println("Yes");
+        }
+	}
+}
