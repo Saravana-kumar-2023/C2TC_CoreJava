229438417d947869320d0a8119eef0ea05f1cc4a
Saravana-kumar-2023 <146946115+Saravana-kumar-2023@users.noreply.github.com>
2024-04-15 13:59:55 +0530

    First Commit

---
+++ src/com/Saravanakumar/daytwo/Travelproblem.java
@@ -0,0 +1,26 @@
+package com.Saravanakumar.daytwo;
+import java.util.Scanner;
+public class Travelproblem {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n = scanner.nextInt();
+		if(n<=1 || n>=1000) {
+			System.out.println("Invalid input ");
+			return ; 
+		}
+		else {
+			for(int i=2;i<n;i++)
+			{
+				int positive = i*10;
+				int negative = -i*10;
+				System.out.println(negative +"\t"+ positive);
+			}
+			
+		}
+		
+
+	}
+
+}
