651a548b814bcd51ce825f3cb97849d23f610606
Saravana-kumar-2023 <146946115+Saravana-kumar-2023@users.noreply.github.com>
2024-04-15 08:19:55 +0530

    First Commit

---
+++ src/com/Saravanakumar/daytwo/CarpeCut.java
@@ -0,0 +1,21 @@
+package com.Saravanakumar.daytwo;
+import java.util.Scanner;
+public class CarpeCut {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of cuts:");
+		int n = scanner.nextInt();
+		int result = maximum_equal_Squares(n);
+		System.out.println("Maximum number of equal squares: " + result);
+		scanner.close();
+	}
+
+	public static int maximum_equal_Squares(int n) {
+		 if (n <= 0) {
+	            return 1; 
+	        } else {
+	        	return 1 + (n * (n + 1)) / 2;
+	        }
+	}
+}
---
+++ src/com/Saravanakumar/daytwo/SuperMarket.java
@@ -0,0 +1,19 @@
+package com.Saravanakumar.daytwo;
+import java.util.Scanner;
+public class SuperMarket {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int product = 1;
+		System.out.println("Enter the number:");
+		int num = scanner.nextInt();
+		while(num!=0) {
+			int digit = num % 10;
+			product = product * digit;
+			num=num/10;
+			
+		}
+		System.out.println("The value is: "+product);
+	}
+
+}
